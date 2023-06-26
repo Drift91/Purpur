fc30537fadb228247b74e1386025580d55f9cbca
Kyle J. McKeown <mckeown_k@hotmail.com>
2023-06-26 15:32:04 -0400

    Update custom builder batch script
    Should fix building between different MC versions

--- Drift_91's Custom Builder.bat
+++ Drift_91's Custom Builder.bat
@@ -4,6 +4,9 @@ cd /d "%~dp0"
 echo Cleaning and resetting repository...
 git clean -fdxq && git reset --hard
 
+echo Clearing Gradle cache...
+gradlew.bat cleanCache --no-daemon
+
 echo Starting gradle...
 gradlew.bat applyPatches --no-daemon && gradlew.bat createReobfPaperclipJar --no-daemon
 
